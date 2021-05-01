3cf07f3884ee2c3b6a7e757d182beed80dcc73a5
Arya Saumitra <aryasaumitra@gmail.com>
2021-05-01 20:03:44 +0530

    Update and Delete Transaction Endpoints created

--- app/server/routes/transaction.py
+++ app/server/routes/transaction.py
@@ -1,6 +1,6 @@
 from ..helper.helper import getISODate, getMonth, getYear
 from ..auth.auth_bearer import JWTBearer
-from fastapi import Body,APIRouter
+from fastapi import Body,APIRouter,status
 from fastapi.encoders import jsonable_encoder
 from fastapi.param_functions import Depends
 from ..auth.auth_handler import decode_jwt
@@ -40,7 +40,7 @@ from ..models.transaction import (
 router= APIRouter()
 
 #Create a transaction
-@router.post("/create",response_description="Transaction added Successfully")
+@router.post("/create",response_description="Transaction added Successfully",status_code=status.HTTP_201_CREATED)
 async def create_transaction(data:TransactionInfoSchema=Body(...),token:str=Depends(JWTBearer())):
     
     userID=decode_jwt(token)
@@ -63,4 +63,38 @@ async def create_transaction(data:TransactionInfoSchema=Body(...),token:str=Depe
 
     if newTransaction:
         return ResponseModel(newTransaction,"Transaction Added Successfully")
-    return ErrorResponseModel("An Error Occured",)
+    return ErrorResponseModel("An Error Occured",status.HTTP_404_NOT_FOUND,"Unable to add transaction")
+
+
+#Update a Transaction
+@router.patch("/update",response_description="Transaction Updated Successfully",status_code=status.HTTP_200_OK)
+async def edit_transaction(transaction_id:str=Body(...),data:TransactionUpdateSchema=Body(...),token:str=Depends(JWTBearer())):
+    
+    updatedTransaction= await update_transaction(transaction_id,data)
+
+    if updatedTransaction:
+        return ResponseModel(updatedTransaction,"Transaction Updated Successfully")
+    return ErrorResponseModel("An Error Occured",status.HTTP_500_INTERNAL_SERVER_ERROR,"Unable to update")
+
+#Delete a Transaction
+@router.delete("/",response_description="Transaction Deleted")
+async def remove_transaction(transaction_id:str=Body(...),token:str=Depends(JWTBearer())):
+
+    deleleTrs= await delete_transaction(transaction_id)
+
+    if deleleTrs:
+        return ResponseModel({"Delete:Successfull"},"Transaction Deleted")
+
+    return ErrorResponseModel("An Error Occured",status.HTTP_500_INTERNAL_SERVER_ERROR,"Unable to delete")
+
+#Delete all Transaction
+@router.delete("/delete",response_description="All Transaction Removed")
+async def remove_all_transaction(token:str=Depends(JWTBearer())):
+
+    userId=decode_jwt(token)
+
+    deleteAllTransaction= await delete_all_transaction(userId['userId'])
+
+    if deleteAllTransaction:
+        return ResponseModel({"Status":"Successfull"},"Deleted all Transaction Successfully")
+    return ErrorResponseModel("An Error Occured",status.HTTP_404_NOT_FOUND,"Unable to delete")
